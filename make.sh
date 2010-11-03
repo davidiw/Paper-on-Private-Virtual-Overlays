087318ed929d10944842e66c4cd0ab2be4498b68
David Wolinsky <davidiw@ufl.edu>
2010-11-03 18:16:38 -0400

    Rewritten, submitted, and accepted at IEEE P2P

--- make.sh
+++ make.sh
@@ -1,7 +1,7 @@
 latex VirtualPrivateOverlays -interaction=nonstopmode
-bibtex VirtualPrivateOverlays
-latex VirtualPrivateOverlays -interaction=nonstopmode
-latex VirtualPrivateOverlays -interaction=nonstopmode
+#bibtex VirtualPrivateOverlays
+#latex VirtualPrivateOverlays -interaction=nonstopmode
+#latex VirtualPrivateOverlays -interaction=nonstopmode
 dvips -t letter VirtualPrivateOverlays.dvi
 ps2pdf VirtualPrivateOverlays.ps
 xpdf VirtualPrivateOverlays.pdf
